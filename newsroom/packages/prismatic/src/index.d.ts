8552545d08e8b1172287d69c85b4545fc1388cda
Duncan Kolba <duncankolba@gmail.com>
2022-12-17 22:34:36 +0100

    WIP

--- newsroom/packages/prismatic/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { PrismaClient } from "@prisma/client";
-export { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-export function createContext(ctx: any): Promise<Context>;
-export * from ".prisma/client/index.d";
-export interface Context {
-  prisma: PrismaClient;
-}
