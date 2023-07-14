6abcbf61ab54b018afe7691df3cf58d0159e8bb7
Duncan Kolba <duncankolba@gmail.com>
2023-07-15 01:12:25 +0200

    WIP

--- packages/prismatic/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-export import PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
-export function createContext(ctx: any): Promise<Context>;
-export * from ".prisma/client/index.d";
-export interface Context {
-  prisma: PrismaClient;
-}
